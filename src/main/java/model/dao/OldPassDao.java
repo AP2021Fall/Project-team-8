3aa1c63cd190b92456f5943029a5bd88d748a6f8
Mohammad Matin Parvin <mohammad123matin@gmail.com>
2021-12-17 12:43:23 +0330

    OldPass Dao implemented

---
+++ src/main/java/model/dao/OldPassDao.java
@@ -0,0 +1,26 @@
+package model.dao;
+
+import model.domain.User;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class OldPassDao extends Dao {
+    /* Static Methods */
+    public static void addOldPass(User user) throws SQLException {
+        String insertOldPass = "insert into `old_pass` (`user_id`, `password`) values (?, ?)";
+        PreparedStatement preparedStatement = preparedStatement(insertOldPass);
+        preparedStatement.setInt(1, user.getId());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+    }
+
+    public static int countOldPassword(User user, String password) throws SQLException {
+        String getOldPassword = "select `password` from `old_pass` where `old_pass`.`user_id` = ? and `old_pass`.`password` = ?";
+
+        PreparedStatement preparedStatement = preparedStatement(getOldPassword);
+        preparedStatement.setInt(1, user.getId());
+        preparedStatement.setString(2, password);
+        return getCount(preparedStatement);
+    }
+}
