2a6df228bbd58e63ee1b4139ca4e1107196f2a0b
MR7-GH <m.gh138080@gmail.com>
2021-12-17 15:47:41 +0330

    chat controll

---
+++ src/main/java/controller/ChatController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import model.dao.ChatDao;
+import model.dao.NotificationDao;
+import model.domain.*;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class ChatController extends Controller {
+    /* Instance Methods */
+    public Respond getChats(Board board) throws Exception {
+        // Success
+        return new Respond(true, ChatDao.getChatsByBoard(board));
+    }
+
+    public Respond sendMessage(User loggedInUser, Board board, String message) throws Exception {
+        // Check For Error
+        if (loggedInUser.getId() != board.getLeader().getId())
+            board.assertContributorExists(loggedInUser.getUsername());
+
+        // Success
+        ChatDao.save(new Chat(board, loggedInUser, Timestamp.from(Instant.now()), message));
+        return new Respond(true, "chat send successfully");
+    }
+
+    public Respond sendNotification(User loggedInUser, Board board, String username, String notifyText) throws Exception {
+        ArrayList<User> sendToUsers = board.getContributors().stream().map(Participant::getUser).collect(Collectors.toCollection(ArrayList::new));
+        User sendToUser;
+
+        // Check For Error
+        authentication(loggedInUser, board);
+
+        // Send To Username (Optional)
+        if (username != null) {
+            sendToUser = board.assertContributorExists(username).getUser();
+            sendToUsers.clear();
+            sendToUsers.add(sendToUser);
+        }
+
+        // Success
+        NotificationDao.sendNotificationToUsers(loggedInUser, notifyText, Timestamp.from(Instant.now()), sendToUsers);
+        return new Respond(true, "notification send successfully");
+    }
+}
